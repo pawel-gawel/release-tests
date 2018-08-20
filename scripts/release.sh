189e58ea0f37a74d32078218e1e9d9be535b18c4
Paweł Kornijów <pawel.kornijow@intive.com>
2018-08-20 23:41:23 +0200

    update

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,5 @@
-owner=$(git config remote.origin.url | sed -n 's/.*:\(.*\)\/.*/\1/p') # "pawel-gawel"
-user=$owner
-repo=$(git config remote.origin.url | sed -n 's/.*\/\(.*\)\.git/\1/p') # "test-utils"
+owner=$(git config remote.origin.url | sed -n 's/.*:\(.*\)\/.*/\1/p')
+repo=$(git config remote.origin.url | sed -n 's/.*\/\(.*\)\.git/\1/p')
 version=$1
 
 while getopts ":hvf" o; do
@@ -24,8 +23,9 @@ shift $((OPTIND-1))
 run() {
   if [ -z $force ]; then
     printf "\n"
-    read -p "This will npm version the repo, push the new commit and tag to Github and create
-new Github release, based on that tag. Are you sure you want to continue [y/n]? " agreed
+    read -p "This will npm version the repo, push the new commit and tag to Github and then go to new release page.
+
+Are you sure you want to continue [y/n]? " agreed
     if [ "$agreed" != "y" ]; then
       printf "\n\tbye!\n\n"; exit
     fi
@@ -37,35 +37,6 @@ new Github release, based on that tag. Are you sure you want to continue [y/n]? 
   git push --tags
 
   open https://github.com/${owner}/${repo}/releases/new?tag=$tag
-}
-
-list() {
-  curl https://api.github.com/repos/${owner}/${repo}/releases
-}
-
-create() {
-  tag=$1
-
-  read -p "Github 2fa OTP code: " otp
-
-  curl \
-    -H "Content-Type: application/json" \
-    -H "X-GitHub-OTP: $otp" \
-    -i \
-    -u $user \
-    -d "{ \"tag_name\": \"$tag\" }" \
-  https://api.github.com/repos/${owner}/${repo}/releases
-}
-
-delete() {
-  release_id=$1
-
-  curl \
-    -X DELETE \
-    -H "X-GitHub-OTP: $otp" \
-    -i \
-    -u pawel-gawel \
-  https://api.github.com/repos/${owner}/${repo}/releases/$release_id
 }
 
 run $version
